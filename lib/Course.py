f6eb39bd2a52adb5b54ffe53823cf634e69cf1ce
benjaminmweribaya <b3njaminbaya@gmail.com>
2025-01-18 21:25:21 +0300

    feat: Scrape a web page's HTML using Beautiful Soup. Use scraped data to give attributes to Python objects. Write an object oriented Scraper class.

--- lib/Course.py
+++ lib/Course.py
@@ -1,2 +1,14 @@
+#!/usr/bin/python
+
 class Course:
-    pass
+    def __init__(self, title, schedule, description):
+        self.title = title
+        self.schedule = schedule
+        self.description = description
+
+    def __str__(self):
+        output = ''
+        return f'Title: {self.title}\nSchedule: {self.schedule}\nDescription: {self.description}\n'
+        output += '------------------'
+        return output 
+        
